57becb1d652c0601341dff400ca19a75eb9e1ee3
sersoong <sersoong@gmail.com>
2019-03-20 13:14:00 +0800

    2.2

---
+++ 2.2/carrots.cpp
@@ -0,0 +1,18 @@
+// carrots.cpp -- food processing program
+// uses and displays a variable
+#include <iostream>
+
+int main(){
+    using namespace std;
+
+    int carrots; //declare an integer variable
+    carrots = 25;//assign a value to the variable
+
+    cout << "I have ";
+    cout << carrots;
+    cout << " carrots.";
+    cout << endl;
+    carrots = carrots - 1;
+    cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+    return 0;
+}
