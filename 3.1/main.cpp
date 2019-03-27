1df2863667f6d9eb20e4c6146354e402e7192814
sersoong <sersoong@gmail.com>
2019-03-27 17:56:00 +0800

    3.1

---
+++ 3.1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <climits>
+
+int main() {
+    using namespace std;
+    int n_int = INT_MAX;
+    short n_short = SHRT_MAX;
+    long n_long = LONG_MAX;
+    long long n_llong = LLONG_MAX;
+
+    cout << "int is " << sizeof(int) << " bytes." << endl;
+    cout << ""
+}
